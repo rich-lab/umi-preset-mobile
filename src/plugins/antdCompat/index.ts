bb7e1b86419c067c473ae4834b3b1d29aa322978
chemdemo <chemdemo@gmail.com>
2022-03-07 14:11:56 +0800

    feat: antdCompat 兼容antd-mobile@2的设置

--- src/plugins/antdCompat/index.ts
+++ src/plugins/antdCompat/index.ts
@@ -3,19 +3,22 @@ import type { IApi } from '@umijs/types';
 import { dirname } from 'path';
 
 /**
- * 关闭@umijs/plugin-antd插件，但开启antd-mobile@5
+ * 关闭@umijs/plugin-antd插件，但开启antd-mobile
  */
 export default (api: IApi) => {
   api.describe({
     key: 'antdCompat',
     config: {
-      schema: (joi) => joi.object(),
-      default: {},
+      schema: (joi) =>
+        joi.object({
+          version: joi.alternatives(2, 5),
+        }),
+      default: {
+        version: 2,
+      },
     },
     // enableBy: api.EnableBy.config, // 用户配置开启
   });
-
-  if (!api.userConfig?.antdCompat) return;
 
   // 跳过antd插件
   api.skipPlugins(['@umijs/plugin-antd']);
@@ -24,34 +27,35 @@ export default (api: IApi) => {
   // @see https://github.com/umijs/plugins/blob/master/packages/plugin-request/src/index.ts#L65
   api.modifyDefaultConfig((memo) => ({ antd: false, ...memo }));
 
-  // 下面是开启antd-mobile的功能，保证antd-mobile的使用不受影响
+  // 下面是开启antd-mobile的功能，保证antd-mobile@2的使用不受影响
+  if (api.userConfig?.antdCompat?.version === 2) {
+    api.chainWebpack((memo) => {
+      // set resolve alias if has aliasName existed
+      const setResolveAlias = (aliasName: string, moduleName: string): void => {
+        if (memo.resolve.alias.has(moduleName)) {
+          memo.resolve.alias.set(aliasName, moduleName);
+        }
+      };
 
-  api.chainWebpack((memo) => {
-    // set resolve alias if has aliasName existed
-    const setResolveAlias = (aliasName: string, moduleName: string): void => {
-      if (memo.resolve.alias.has(moduleName)) {
-        memo.resolve.alias.set(aliasName, moduleName);
-      }
-    };
+      setResolveAlias('@umijs/antd-mobile', 'antd-mobile');
 
-    setResolveAlias('@umijs/antd-mobile', 'antd-mobile');
+      return memo;
+    });
 
-    return memo;
-  });
+    api.modifyBabelPresetOpts((opts) => {
+      return {
+        ...opts,
+        import: (opts.import || []).concat([
+          { libraryName: 'antd-mobile', libraryDirectory: 'es', style: true },
+        ]),
+      };
+    });
 
-  api.modifyBabelPresetOpts((opts) => {
-    return {
-      ...opts,
-      import: (opts.import || []).concat([
-        { libraryName: 'antd-mobile', libraryDirectory: 'es', style: true },
-      ]),
-    };
-  });
-
-  api.addProjectFirstLibraries(() => [
-    {
-      name: 'antd-mobile',
-      path: dirname(require.resolve('antd-mobile/package.json')),
-    },
-  ]);
+    api.addProjectFirstLibraries(() => [
+      {
+        name: 'antd-mobile',
+        path: dirname(require.resolve('antd-mobile/package.json')),
+      },
+    ]);
+  }
 };
